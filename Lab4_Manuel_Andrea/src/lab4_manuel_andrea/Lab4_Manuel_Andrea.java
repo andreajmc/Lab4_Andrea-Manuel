e4e29969cb32ddde47818af5fe8a6e2622990943
The AJ <andreaj@unitec.edu>
2018-08-10 13:45:10 -0600

    whatever

--- Lab4_Manuel_Andrea/src/lab4_manuel_andrea/Lab4_Manuel_Andrea.java
+++ Lab4_Manuel_Andrea/src/lab4_manuel_andrea/Lab4_Manuel_Andrea.java
@@ -16,6 +16,7 @@ public class Lab4_Manuel_Andrea {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
