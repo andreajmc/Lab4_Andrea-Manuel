bf6d62d40d119e80e023636c86c95256cc0f4e4a
mevl1827 <mevl1827@hotmail.com>
2018-08-10 18:23:12 -0600

    merging

--- Lab4_Manuel_Andrea/src/lab4_manuel_andrea/EVA02.java
+++ Lab4_Manuel_Andrea/src/lab4_manuel_andrea/EVA02.java
@@ -4,8 +4,15 @@ public class EVA02 extends EVA {
 
     public boolean movimiento(int x, int y, int x_actual, int y_actual) {
         boolean t=false;
+        if (((x==x_actual+1)||(x==x_actual-1))&& ((y==y_actual+2) || (y==y_actual-2))) {
+            t=true;
+        }
+        if (((x==x_actual+2)||(x==x_actual-2))&& ((y==y_actual+1) || (y==y_actual-1))) {
+            t=true;
+        }
+            
         
-        return false;
+        return t;
     }
 
     public boolean ataque(int x, int y, int x_actual, int y_actual) {
--- Lab4_Manuel_Andrea/src/lab4_manuel_andrea/SACHIEL.java
+++ Lab4_Manuel_Andrea/src/lab4_manuel_andrea/SACHIEL.java
@@ -27,6 +27,21 @@ public class SACHIEL extends ANGELES {
     @Override
       public boolean  ataque(int x,int y , int x_actual,int y_actual){
           boolean t=false;
+          if (y==y_actual && x==x_actual+1) {
+            t=true;
+        }
+          if (y==y_actual && x==x_actual-1) {
+            t=true;
+        }
+          if (y+x==y_actual+x_actual) {
+            t=true;
+        }
+          if (x-y==x_actual-y_actual) {
+            t=true;
+        }
+          if (y-x==y_actual-x_actual) {
+            t=true;
+        }
           return t;
       }
     
